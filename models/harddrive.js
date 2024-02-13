eac446f397d146b7f55db911356fd02fbaebdfe3
connorreidy1 <connorjreidy@gmail.com>
2024-02-13 13:11:59 -0700

    img_path added to models

--- models/harddrive.js
+++ models/harddrive.js
@@ -7,10 +7,11 @@ const harddriveSchema = new mongoose.Schema(
         capacity: {type: String, required: true},
         make_year: {type: Number, required: true},
         storage_type: {type: String, required: true},
-        intergace: {type: String, required: true},
+        interface: {type: String, required: true},
         max_speed: {type: String, required: true},
         model_num: {type: String, required: true},
-        price: {type: Number, required: true}
+        price: {type: Number, required: true},
+        img_path: {type: String, required: true},
     },
     {timestamps: true}
 )
--- models/spec.js
+++ models/spec.js
@@ -5,8 +5,9 @@ const specSchema = new mongoose.Schema(
         chip: {type: String, required: true},
         memory: {type: String, required: true},
         size_in_inches: {type: String, required: true},
-        year: {type: String, required: true},
+        year: {type: Number, required: true},
         price: {type: Number, required: true},
+        img_path: {type: String, required: true},
         computerId: {type: mongoose.Schema.Types.ObjectId, ref: 'Computer'},
     },
     {timestamps: true}
