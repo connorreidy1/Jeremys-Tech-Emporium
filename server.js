e35fc4631d9fb8052a19b4cc7ae6a650c3511a20
connorreidy1 <connorjreidy@gmail.com>
2024-02-13 21:04:14 -0700

    init y, mongooose, expresss, nodemon, cors, morgan, body-parser installs, db.index.js and server.js created

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const db = require('./db')
+const cors = require('cors')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3001
+
+const computerController = require('./controllers/computerController')
+const harddriveController = require('./controllers/harddriveController')
+const specController = require('./controllers/specController')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`)
+})
+
+
+app.get('/', (req, res) => {
+    res.send("Jeremy's Computer Emporium! Let's sell come hardware!")
+  })
